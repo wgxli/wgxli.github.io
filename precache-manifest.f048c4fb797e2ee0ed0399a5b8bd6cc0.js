deaddd099d574fe7e431d6ba2166eab598cb737f
Samuel Li <samuel.wgx@gmail.com>
2018-12-16 15:56:41 -0500

    Updates

---
+++ precache-manifest.f048c4fb797e2ee0ed0399a5b8bd6cc0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4aa5835042b60aff054b18de261ecaf8",
+    "url": "/static/media/banner-image-compressed.4aa58350.jpg"
+  },
+  {
+    "revision": "5d3fb38727a478efc407",
+    "url": "/static/css/main.2728249e.chunk.css"
+  },
+  {
+    "revision": "e7e383648875bf0d99f2",
+    "url": "/static/js/1.e7e38364.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da3d15378cffc0fca0fc915eef92ddcb",
+    "url": "/static/media/down-arrow.da3d1537.svg"
+  },
+  {
+    "revision": "063c2e8393009c6a0cb5314a3db92b23",
+    "url": "/static/media/toronto-blurred.063c2e83.jpg"
+  },
+  {
+    "revision": "5d3fb38727a478efc407",
+    "url": "/static/js/main.5d3fb387.chunk.js"
+  },
+  {
+    "revision": "62367fef543703541320e221fa7b3c03",
+    "url": "/static/media/github.62367fef.svg"
+  },
+  {
+    "revision": "02b940201e6b2ee3204aa1bd6e7a0f2c",
+    "url": "/static/media/linkedin.02b94020.svg"
+  },
+  {
+    "revision": "2f708490fb26237452a106d21a297c94",
+    "url": "/static/media/complex-function-plotter-compressed.2f708490.jpg"
+  },
+  {
+    "revision": "adda1f0b85bdfb0cf5e400c3ac134c37",
+    "url": "/static/media/quantum-simulator-compressed.adda1f0b.png"
+  },
+  {
+    "revision": "ae81953b251c6382cd01f5f32d615c39",
+    "url": "/static/media/simpletex-compressed.ae81953b.png"
+  },
+  {
+    "revision": "bf22ad1549aaee958a0e2f30285fbe8e",
+    "url": "/index.html"
+  }
+];
